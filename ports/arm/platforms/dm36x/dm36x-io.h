b367bbbde8aa48bfb744a96e109c3dd85a83b91a
Kelvin Lawson <kelvin.a.lawson@gmail.com>
2014-06-28 14:57:59 +0100

    edma3: Add more register macros

--- ports/arm/platforms/dm36x/dm36x-io.h
+++ ports/arm/platforms/dm36x/dm36x-io.h
@@ -174,12 +174,13 @@
 
 /** System registers */
 #define DM36X_SYSTEM_BASE       0x01C40000 /* System base registers */
-#define DM36X_SYSTEM_PINMUX0    0x00
-#define DM36X_SYSTEM_PINMUX1    0x04
-#define DM36X_SYSTEM_PINMUX2    0x08
-#define DM36X_SYSTEM_PINMUX3    0x0C
-#define DM36X_SYSTEM_PINMUX4    0x10
-#define DM36X_SYSTEM_PERICLKCTL 0x48
+#define DM36X_SYSTEM_PINMUX0        0x00
+#define DM36X_SYSTEM_PINMUX1        0x04
+#define DM36X_SYSTEM_PINMUX2        0x08
+#define DM36X_SYSTEM_PINMUX3        0x0C
+#define DM36X_SYSTEM_PINMUX4        0x10
+#define DM36X_SYSTEM_EDMA_EVTMUX    0x1C
+#define DM36X_SYSTEM_PERICLKCTL     0x48
 /* PINMUX0 register bitfields */
 #define DM36X_PINMUX0_MMCSD0     24
 #define DM36X_PINMUX0_GIO49      23
