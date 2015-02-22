5033171ac0866192c483b1928809389720cdfdc2
Kelvin Lawson <kelvin.a.lawson@gmail.com>
2015-02-22 02:27:13 +0000

    Commit hook to build and run qemu-arm and AVR tests.

---
+++ tests/commit-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+# Quit on any error
+set -e
+
+# Start at top-level directory
+cd ..
+
+# Build avr and run tests
+cd ports/avr
+make clean
+make PART=atmega128
+make PART=atmega128 simtests
+cd ../..
+
+# Build qemu-arm and run tests
+cd ports/arm/platforms/qemu_integratorcp
+make clean
+make
+make qemutests
+
