f9d5f0a10ccef59029a135779cbe3261224cbbb0
Jacques Germishuys <jacquesg@striata.com>
2017-08-04 21:02:44 +0200

    use F_CPU for the AVR CPU frequency (if available)

--- ports/avr/atomport-private.h
+++ ports/avr/atomport-private.h
@@ -31,7 +31,11 @@
 #define __ATOM_PORT_PRIVATE_H
 
 /* CPU Frequency */
+#ifdef F_CPU
+#define AVR_CPU_HZ          F_CPU
+#else
 #define AVR_CPU_HZ          1000000
+#endif
 
 #ifdef __cplusplus
 extern "C" {
